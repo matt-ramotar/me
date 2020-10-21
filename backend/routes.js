549d6d48a64846f5d668877168680fa46998656a
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-20 18:46:33 -0700

    Add skills routes

--- backend/routes.js
+++ backend/routes.js
@@ -4,16 +4,75 @@ const { docId, tableIds, token } = require('./env');
 
 const coda = new Coda(token);
 
+router.get('/tables/ids', async (req, res) => {
+  const doc = await coda.getDoc(docId);
+  const tables = await doc.listTables();
+  const tableIds = {};
+  for (const table of tables) {
+    tableIds[table.name] = table.id;
+  }
+  return res.json(tableIds);
+});
+
 router.get('/projects', async (req, res) => {
   const doc = await coda.getDoc(docId);
-
   const projectsTable = await doc.getTable(tableIds.projects);
-  const projects = await projectsTable.listRows({ useColumnNames: true });
-  const response = [];
-  for (const project of projects) {
-    response.push(project.values);
+  const projectsRows = await projectsTable.listRows({ useColumnNames: true });
+  const projects = [];
+  for (const projectRow of projectsRows) {
+    projects.push(projectRow.values);
   }
-  return res.json(response);
+  return res.json(projects);
+});
+
+router.get('/projects/tech', async (req, res) => {
+  const doc = await coda.getDoc(docId);
+  const projectsTable = await doc.getTable(tableIds.projects);
+  const projectsRows = await projectsTable.listRows({ useColumnNames: true });
+  const projects = [];
+  for (const projectRow of projectsRows) {
+    if (projectRow.values.github) {
+      projects.push(projectRow.values);
+    }
+  }
+  return res.json(projects);
+});
+
+router.get('/skills', async (req, res) => {
+  const doc = await coda.getDoc(docId);
+  const skillsTable = await doc.getTable(tableIds.skills);
+  const skillsRows = await skillsTable.listRows({ useColumnNames: true });
+  const skills = [];
+  for (const skillRow of skillsRows) {
+    skills.push(skillRow);
+  }
+  return res.json(skills);
+});
+
+router.get('/skills/languages', async (req, res) => {
+  const doc = await coda.getDoc(docId);
+  const skillsTable = await doc.getTable(tableIds.skills);
+  const skillsRows = await skillsTable.listRows({ useColumnNames: true });
+  const skills = [];
+  for (const skillRow of skillsRows) {
+    if (skillRow.values.type.includes('languages')) {
+      skills.push(skillRow.values.skill);
+    }
+  }
+  return res.json(skills.sort());
+});
+
+router.get('/skills/tech', async (req, res) => {
+  const doc = await coda.getDoc(docId);
+  const skillsTable = await doc.getTable(tableIds.skills);
+  const skillsRows = await skillsTable.listRows({ useColumnNames: true });
+  const skills = [];
+  for (const skillRow of skillsRows) {
+    if (skillRow.values.type.includes('technologies')) {
+      skills.push(skillRow.values.skill);
+    }
+  }
+  return res.json(skills.sort());
 });
 
 module.exports = router;
