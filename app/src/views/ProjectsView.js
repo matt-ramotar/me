515a3bdf836b6f7064c54d0912dbeab4b82830da
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-20 18:45:39 -0700

    Display project cards

--- app/src/views/ProjectsView.js
+++ app/src/views/ProjectsView.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProjects } from '../store/projects';
+import ProjectCard from './ProjectCard';
 
 export function ProjectsView({ getProjectsDispatcher, projects }) {
   useEffect(() => async => {
@@ -16,6 +17,21 @@ export function ProjectsView({ getProjectsDispatcher, projects }) {
           <h1>{project.project}</h1>
           <h2>{project.year}</h2>
           <h2>{project.location}</h2>
+          <h2>{project.github}</h2>
+          <ProjectCard
+            name={project.project}
+            year={project.year}
+            location={project.location}
+            github={project.github}
+            skills={project.skills}
+            logo={project.logo}
+            banner={project.banner}
+          />
+          <div>
+            {project['skills-img'].split(',').map(src => {
+              return <img src={src} height='28px' width='28px' />;
+            })}
+          </div>
           <li>
             {project.skills.split(',').map(skill => (
               <div>
