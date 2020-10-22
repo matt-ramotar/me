e22a7599eecb279a7fb1c592e91eaa31c62c0c23
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-22 06:18:51 -0700

    Add footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
 import { Box, Typography } from '@material-ui/core/';
-import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-  link: {
-    textDecoration: 'none',
-  },
-}));
+import { SocialProvider, SocialLink } from '@mui-treasury/components/socialLink';
 
 export default function Footer() {
-  const classes = useStyles();
-  const handleClick = e => {
-    e.preventDefault();
-  };
   return (
-    <Box color='white'>
-      <Link to='/#about' className={classes.link}>
-        <Typography variant='h6'>Tag</Typography>
-      </Link>
-      <Link to='/#projects' className={classes.link}>
-        <Typography variant='h6'>Tag</Typography>
-      </Link>
-      <Link to='/#contact' className={classes.link}>
-        <Typography variant='h6'>Contact</Typography>
-      </Link>
+    <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <Box style={{ display: 'flex' }}>
+        <SocialProvider>
+          <SocialLink brand={'Envelope'} />
+          <SocialLink brand={'GithubCircle'} />
+          <SocialLink brand={'LinkedIn'} />
+          <SocialLink brand={'Twitter'} />
+        </SocialProvider>
+      </Box>
+
+      <Box style={{ display: 'flex' }}>
+        <Typography variant='h6' color='textSecondary'>
+          Built with ❤️ in Portland
+        </Typography>
+      </Box>
     </Box>
   );
 }
