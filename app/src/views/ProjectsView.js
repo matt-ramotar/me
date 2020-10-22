bafafa19a5ed954409897dc1e289c3bfce3018eb
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-22 11:01:55 -0700

    Refactor

--- app/src/views/ProjectsView.js
+++ app/src/views/ProjectsView.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Grid, CardHeader } from '@material-ui/core';
+import { Box, CardHeader, Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 import { getProjects } from '../store/projects';
 import ProjectCard from './ProjectCard';
@@ -15,6 +18,28 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+  },
+  gridListRoot: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: 'transparent',
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+    height: 650,
+  },
+  tile: {
+    height: '100% !important',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background: 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
 }));
 
@@ -29,22 +54,25 @@ export function ProjectsView({ getProjectsDispatcher, projects }) {
 
   return (
     <>
-      <h1 id='projects'>Projects</h1>
-      <Grid container spacing={6}>
-        {projects.map(project => (
-          <Grid item xs={4}>
-            <ProjectCard
-              name={project.project}
-              year={project.year}
-              location={project.location}
-              github={project.github}
-              skills={project.skills}
-              logo={project.logo}
-              banner={project.banner}
-            />
-          </Grid>
-        ))}
-      </Grid>
+      <Box className={classes.gridListRoot} id='projects'>
+        <GridList className={classes.gridList} cols={3.5}>
+          {projects.map(project => (
+            <GridListTile className={classes.tile}>
+              {/* <img src={project.banner} /> */}
+              {/* <GridListTileBar title={project.project} className={(classes.titleBar, classes.title)} /> */}
+              <ProjectCard
+                name={project.project}
+                year={project.year}
+                location={project.location}
+                github={project.github}
+                skills={project.skills}
+                logo={project.logo}
+                banner={project.banner}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </Box>
     </>
   );
 }
